db066206230271dcd63e61aaf27dd25b0e4a5051
Thiago <t.macahyba@gmail.com>
2020-05-27 20:15:27 -0300

    v1.4.2
    Add https/SSL support

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  http_host: 'http://ec2-13-58-84-178.us-east-2.compute.amazonaws.com',
-  api_host: 'http://ec2-13-58-84-178.us-east-2.compute.amazonaws.com:8080/api'
+  http_host: 'https://portalinvoice.tk',
+  api_host: 'https://portalinvoice.tk:8443/api'
 };
