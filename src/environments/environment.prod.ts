8c551ec344a0a1b543a642913198a2547b722c0f
Thiago <t.macahyba@gmail.com>
2020-06-16 17:45:10 -0300

    v1.4.4
    Fix https port due to firewall issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   http_host: 'https://portalinvoice.tk',
-  api_host: 'https://portalinvoice.tk:8443/api'
+  api_host: 'https://portalinvoice.tk:8080/api'
 };
